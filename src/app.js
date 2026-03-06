4eeb23f34bf42d47d61f83c8e49530d7dd2936a8
Naveen <naveenchandel7654@gmail.com>
2026-03-06 10:09:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ import bookingRoute from "./modules/booking/booking.route.js";
 
 const app = express();
 
+// middleware
 app.use(express.json());
 
+// routes
 app.use("/api", bookingRoute);
+
+// health check route (helps test server)
+app.get("/", (req, res) => {
+  res.send("Redis Seat Locking API Running");
+});
 
 export default app;
